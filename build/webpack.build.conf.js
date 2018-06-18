dc8613cd83124a931440dfacad2d4b6f105b27f8
qweaa <2275414653@qq.com>
2018-06-19 00:58:26 +0800

    xcl

---
+++ build/webpack.build.conf.js
@@ -0,0 +1,64 @@
+var path = require('path')
+var webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: './packages/index.js',
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/dist/',
+        filename: 'myUI.min.js',
+        library: 'myUI',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    externals: {
+        vue: {
+            root: 'Vue',
+            commonjs: 'vue',
+            commonjs2: 'vue',
+            amd: 'vue'
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader',
+            options: {
+                loaders: {}
+                // other vue-loader options go here
+            }
+        },{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        },{
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]?[hash]'
+            }
+        },{ 
+            test: /\.less$/i,
+            use: ExtractTextPlugin.extract([ 'css-loader', 'less-loader' ])
+        },]
+    },
+    performance: {
+        hints: false
+    },
+    plugins: [
+        new ExtractTextPlugin('./myUI.min.css'),
+        new UglifyJsPlugin({
+            uglifyOptions: {
+                compress: {
+                    warnings: false
+                }
+            },
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+        })
+    ]
+}
+
--- packages/index.js
+++ packages/index.js
@@ -1,4 +1,5 @@
 import Button from './components/button';
+import './index.less'
 
 const components = {
     Button,
